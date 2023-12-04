e6a44dfd3e97f5215c4aadb6e0c16a45f698e8bb
asiek728 <asiek728@gmail.com>
2023-12-04 16:55:35 +0000

    add more songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import banner from './assets/imagineDragons.png'
 import './App.css'
 import { Like } from './Like'
+import { ArtistInfo } from './ArtistInfo'
 
 function App() {
   const songs = [
     { title: 'Believer', id: 1, imageUrl: 'https://upload.wikimedia.org/wikipedia/en/5/5c/Imagine-Dragons-Believer-art.jpg', imageSize: 200, date: "2017", liked: false },
     { title: 'Radioactive', id: 2, imageUrl: 'https://i1.sndcdn.com/artworks-000058618127-o9ql4a-t500x500.jpg', imageSize: 200, date: "2012", liked: true },
     { title: 'Enemy', id: 3, imageUrl: 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/0cd265130636097.61842df318d10.jpg', imageSize: 200, date: "2021", liked: false },
+    { title: 'Demons', id: 4, imageUrl: 'https://upload.wikimedia.org/wikipedia/en/2/2b/Imagine_Dragons_-_%22Demons%22_%28Official_Single_Cover%29.jpg', imageSize: 200, date: "2012", liked: false },
+    { title: 'Symphony', id: 5, imageUrl: 'https://i.scdn.co/image/ab67616d00001e02615c5525475415c56dadc6ef', imageSize: 200, date: "2022", liked: false },
+    { title: 'Battle Cry', id: 6, imageUrl: 'https://i.ytimg.com/vi/ZgB2qItM76g/maxresdefault.jpg', imageSize: 200, date: "2015", liked: false },
+    { title: 'Thunder', id: 7, imageUrl: 'https://i1.sndcdn.com/artworks-000635076223-98cavr-t500x500.jpg', imageSize: 200, date: "2017", liked: false },
+    { title: 'Bad Liar', id: 8, imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Imagine_Dragons_Bad_Liar.jpg/220px-Imagine_Dragons_Bad_Liar.jpg', imageSize: 200, date: "2018", liked: false },
+
   ];
 
   const [myList, setMyList] = useState(songs);
@@ -59,13 +65,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://www.imaginedragonsmusic.com/#/" target="_blank">
-          <img src={banner} className="banner" alt="Imagine Dragons photo" />
-        </a>
-      </div>
-      <h1>Imagine Dragons</h1>
-      <p>Imagine Dragons is an American pop rock band based in Las Vegas, Nevada in 2008, and currently consists of lead singer Dan Reynolds, guitarist Wayne Sermon, bassist Ben McKee and drummer Daniel Platzman. The band first gained exposure with the release of their single "It's Time", followed by their debut album Night Visions (2012), which resulted in the chart-topping singles "Radioactive" and "Demons". Rolling Stone named "Radioactive", which held the record for most weeks charted on the Billboard Hot 100, the "biggest rock hit of the year". MTV called them "the year's biggest breakout band", and Billboard named them their "Breakthrough Band of 2013" and "Biggest Band of 2017", and placed them at the top of their "Year in Rock" rankings for 2013, 2017, and 2018. Imagine Dragons topped the Billboard Year-End "Top Artists – Duo/Group" category in 2018.</p>
+      <ArtistInfo />
+
       <div className="songsDiv">
         {listSongs}
       </div>
---
+++ src/ArtistInfo.jsx
@@ -0,0 +1,16 @@
+import banner from './assets/imagineDragons.png'
+
+export const ArtistInfo = () => {
+
+    return (
+        <>
+            <div>
+                <a href="https://www.imaginedragonsmusic.com/#/" target="_blank">
+                    <img src={banner} className="banner" alt="Imagine Dragons photo" />
+                </a>
+            </div>
+            <h1>Imagine Dragons</h1>
+            <p>Imagine Dragons is an American pop rock band based in Las Vegas, Nevada in 2008, and currently consists of lead singer Dan Reynolds, guitarist Wayne Sermon, bassist Ben McKee and drummer Daniel Platzman. The band first gained exposure with the release of their single "It's Time", followed by their debut album Night Visions (2012), which resulted in the chart-topping singles "Radioactive" and "Demons". Rolling Stone named "Radioactive", which held the record for most weeks charted on the Billboard Hot 100, the "biggest rock hit of the year". MTV called them "the year's biggest breakout band", and Billboard named them their "Breakthrough Band of 2013" and "Biggest Band of 2017", and placed them at the top of their "Year in Rock" rankings for 2013, 2017, and 2018. Imagine Dragons topped the Billboard Year-End "Top Artists – Duo/Group" category in 2018.</p>
+        </>
+    )
+}
