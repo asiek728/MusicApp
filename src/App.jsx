4c8d2af7fb9573b9c61b08799bea9eabb4d7ad23
asiek728 <asiek728@gmail.com>
2023-12-04 11:10:51 +0000

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import banner from './assets/imagineDragons.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,14 +9,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.imaginedragonsmusic.com/#/" target="_blank">
+          <img src={banner} className="banner" alt="Imagine Dragons photo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Imagine Dragons</h1>
+      <p>Imagine Dragons is an American pop rock band based in Las Vegas, Nevada in 2008, and currently consists of lead singer Dan Reynolds, guitarist Wayne Sermon, bassist Ben McKee and drummer Daniel Platzman. The band first gained exposure with the release of their single "It's Time", followed by their debut album Night Visions (2012), which resulted in the chart-topping singles "Radioactive" and "Demons". Rolling Stone named "Radioactive", which held the record for most weeks charted on the Billboard Hot 100, the "biggest rock hit of the year". MTV called them "the year's biggest breakout band", and Billboard named them their "Breakthrough Band of 2013" and "Biggest Band of 2017", and placed them at the top of their "Year in Rock" rankings for 2013, 2017, and 2018. Imagine Dragons topped the Billboard Year-End "Top Artists – Duo/Group" category in 2018.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
